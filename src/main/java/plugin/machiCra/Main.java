9d3da814eec27f7bff060bf783426972d8b52270
mhsaya22 <satoru.sayaka07@gmail.com>
2025-07-02 19:04:22 +0900

    UUIDの取得

--- src/main/java/plugin/machiCra/Main.java
+++ src/main/java/plugin/machiCra/Main.java
@@ -5,6 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import plugin.machiCra.command.MachiCraCommand;
+import plugin.machiCra.command.NewMachiCraCommand;
 
 public final class Main extends JavaPlugin implements Listener{
 
@@ -12,5 +13,8 @@ public final class Main extends JavaPlugin implements Listener{
   public void onEnable(){
     Bukkit.getPluginManager().registerEvents(this,this);
     getCommand("machiCra").setExecutor(new MachiCraCommand());
+
+    Bukkit.getPluginManager().registerEvents(this,this);
+    getCommand("newMachiCra").setExecutor(new NewMachiCraCommand());
   }
 }
--- src/main/java/plugin/machiCra/command/NewMachiCraCommand.java
+++ src/main/java/plugin/machiCra/command/NewMachiCraCommand.java
@@ -1,20 +1,22 @@
 package plugin.machiCra.command;
 
-
+import java.io.File;
 import java.util.UUID;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 
-public class MachiCraCommand implements CommandExecutor {
-
+public class NewMachiCraCommand implements CommandExecutor {
 
   @Override
   public boolean onCommand (CommandSender sender, Command command, String label, String[] args){
-    if (sender instanceof Player player) {
+    if (sender instanceof Player player){
       UUID uuid = player.getUniqueId();
+      File dataFolder = new File(getPlugin().getDataFolder(),"playerData")
     }
     return false;
   }
+
 }
